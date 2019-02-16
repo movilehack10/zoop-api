ae8dcd379009ea3c7658c3086e80845729ddf828
Carolina Junqueira <junqueiracarolina@live.com>
2019-02-16 21:22:10 -0200

    correcao porta

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-// const mapCtrl = require("./controllers/map-controller");
+const zoop = require("./zoop-controller");
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:false}));
@@ -10,6 +10,6 @@ app.get('/', function(req,res){
   res.send("Rota root");
 })
 
-app.listen(3000,function(){
-  console.log("Ouvindo a porta 3000!");
+app.listen(3004,function(){
+  console.log("Ouvindo a porta 3004!");
 })
