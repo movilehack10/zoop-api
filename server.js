147c9cb6518dad0a2e13b2f5b4065517a1179045
Carolina Junqueira <junqueiracarolina@live.com>
2019-02-17 01:39:12 -0300

    associacao cartao

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const zoopCtrl = require("./zoop-controller");
 require('dotenv').config();
 
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', function(req,res){
   zoopCtrl.newWallet()
@@ -13,9 +13,36 @@ app.get('/', function(req,res){
       res.send(response);
     })
     .catch((err) => {
-      res.err(err);
+      res.err(new Error({message:'server error', status: 500}));
     })
 })
+
+app.post('/credit-transaction', function(req, res){
+  if(!req.body.buyer_id || !req.body.amount){
+    res.err(new Error({message:'params error', status: 400}));
+  }else{
+    zoopCtrl.createCreditTransaction(req.body.buyer_id,req.body.amount)
+      .then((response) => {
+        if(response.error){
+          res.send(response.error);
+        }else{
+          res.send(response);
+        }
+      })
+      .catch((err) =>{
+        res.err(new Error({message:'server error', status: 500}));
+      })
+  }
+});
+
+// app.post('/associate-card-customer', function(req, res){
+//   if(!req.body.buyer_id || !req.body.card_token){
+//     res.err(new Error({message:'params error', status: 400}));
+//   }else{
+//     res.send("MAOE");
+//     // zoopCtrl.associateCardCustomer()
+//   }
+// });
 
 app.listen(3004,function(){
   console.log("Ouvindo a porta 3004!");
--- zoop-controller.js
+++ zoop-controller.js
@@ -1,11 +1,13 @@
 var request = require("request");
 
 function newWallet(){
-  return newBuyer();
+  return new newBuyer();
 }
 
+// create buyer
 function newBuyer(){
-  let auth = "Basic " + new Buffer(process.env.PUB_KEY + ": ").toString("base64");
+  const auth = "Basic " + new Buffer(process.env.PUB_KEY + ": ").toString("base64");
+
   let options = {
     headers : {
       "Authorization": auth,
@@ -22,6 +24,76 @@ function newBuyer(){
         reject(error);
       }
       else {
+        resolve(body.id);
+      }
+    });
+  })
+}
+
+// associate credit card and customer
+function associateCardCustomer(buyer_id, card_token){
+  const auth = "Basic " + new Buffer(process.env.PUB_KEY + ": ").toString("base64");
+
+  let options = {
+    headers : {
+      "Authorization": auth,
+      "Content-type": "application/json"
+    },
+    method: 'POST',
+    url: `https://api.zoop.ws/v1/marketplaces/${process.env.MARKETPLACE_ID}/cards`,
+    json: {
+      "token": card_token,
+      "customer": buyer_id
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    request(options, function (error, response, body){
+      if (error){
+        reject(error);
+      }
+      else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+// card not present transaction
+function createCreditTransaction(buyer_id, amount){
+  const auth = "Basic " + new Buffer(process.env.PUB_KEY + ": ").toString("base64");
+  let options = {
+    headers : {
+      "Authorization": auth,
+      "Content-type": "application/json"
+    },
+    method: 'POST',
+    url: `https://api.zoop.ws/v1/marketplaces/${process.env.MARKETPLACE_ID}/transactions`,
+    json: {
+    	"amount": amount,
+      "currency": "BRL",
+      "description": "venda",
+      "on_behalf_of": process.env.SELLER_MASTER,
+      "payment_type": "credit",
+      "source": {
+        "usage": "single_use",
+    		"amount": amount,
+    		"currency": "BRL",
+    		"description": "TestTest",
+    		"type": "card",
+    		"card": {
+    		  "id": process.env.INFINITE_CARD
+        }
+      }
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    request(options, function (error, response, body){
+      if (error){
+        reject(error);
+      }
+      else {
         resolve(body);
       }
     });
@@ -29,5 +101,7 @@ function newBuyer(){
 }
 
 module.exports = {
-  newWallet: newWallet
+  newWallet: newWallet,
+  createCreditTransaction: createCreditTransaction,
+  associateCardCustomer: associateCardCustomer
 };
